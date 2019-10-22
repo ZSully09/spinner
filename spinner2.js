332771e4bd0a28798995204fb172f1cdb8370964
ZSully09 <zsullivan93@gmail.com>
2019-10-22 22:46:02 +0000

    refactoring to loop through an array and increment up by 200ms after the initial 100ms delay

--- spinner2.js
+++ spinner2.js
@@ -1,37 +1,22 @@
-setTimeout(() => {
-  process.stdout.write("\r|   ");
-}, 100);
+const stdOut = process.stdout;
+let counter = 100;
+let i = 0;
+let offset = 200;
 
-setTimeout(() => {
-  process.stdout.write("\r/   ");
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write("\r-   ");
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write("\r\\   ");
-}, 700);
-
-// ... fill in the rest yourself ...
-setTimeout(() => {
-  process.stdout.write("\r|   ");
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write("\r/   ");
-}, 1100);
-
-setTimeout(() => {
-  process.stdout.write("\r-   ");
-}, 1300);
-
-setTimeout(() => {
-  process.stdout.write("\r\\   ");
-}, 1500);
-
-setTimeout(() => {
-  process.stdout.write("\r|   \n");
-}, 1700);
+const array = [
+  "|   ",
+  "/   ",
+  "-   ",
+  "\\   ",
+  "|   ",
+  "/   ",
+  "-   ",
+  "\\   ",
+  "|   \n"
+];
+for (const item of array) {
+  setTimeout(() => {
+    stdOut.write("\r" + item);
+  }, counter + i * offset);
+  i++;
+}
