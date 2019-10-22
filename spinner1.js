1835760bc697b88d7b5a78ae3cf9cf49ec1b08e6
ZSully09 <zsullivan93@gmail.com>
2019-10-22 21:36:53 +0000

    removed replicate comments

--- spinner1.js
+++ spinner1.js
@@ -29,11 +29,9 @@ setTimeout(() => {
 }, 1300);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r\\   ");
 }, 1500);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r|   \n");
 }, 1700);
